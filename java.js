3643fcf65500c5c9a131ab22d81e770e2c943b3b
kira <arooji25@live.com>
2023-11-13 11:18:13 +0300

    add

---
+++ java.js
@@ -0,0 +1,37 @@
+function api(){
+    fetch('https://fakestoreapi.com/products',{
+      method: "get"
+    })
+      
+       .then(response=>response.json())
+       .then(data =>{
+           for(let i=0; i<data.length;i++){
+              console.log(data.length)
+              let getData=document.getElementById('container')
+              let res=document.createElement('div')
+              res.className="box"
+              res.style.fontWeight='bolder'
+              res.style.textShadow='1px 0px 2px  black'
+              res.innerHTML=data[i].title+'.'
+              let price=document.createElement('div')
+              price.innerHTML+=' Price: '+data[i].price +'$'
+              price.style.color='green'
+              let pic=document.createElement('img')
+              pic.src=data[i].image
+              res.appendChild(price)
+              res.appendChild(pic)
+              getData.appendChild(res)
+              
+  
+           }
+  
+  
+  
+       })
+  
+  
+  }
+  
+  api() 
+  
+  
